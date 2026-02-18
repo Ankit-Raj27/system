d5ae12573606b97a9e6158652d8592b2e0971e1d
Ankit Raj Kushwaha <ankitrajk27@gmail.com>
2026-02-18 11:10:01 +0530

    Updates

--- _next/static/a6nkeoZuejig6vsc9yzf2/_buildManifest.js
+++ _next/static/a6nkeoZuejig6vsc9yzf2/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/system//_next/:path+",
+        "destination": "/system/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
